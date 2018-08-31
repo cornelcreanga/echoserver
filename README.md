4eae470cbeae98730fcf46bcd76a25891cf11000
creanga@adobe.com <creanga@adobe.com>
2018-08-31 17:48:32 +0300

    cleaning up unused imports

--- README.md
+++ README.md
@@ -1,4 +1,1 @@
-# echoserver - simple integration between github,travis, beanstalk
-
-todo - cloud formation eg:
-https://github.com/gkatzioura/AWSJavaDeploy/blob/master/BeanstalkDeployment/beanstalkspring.template
+# echoserver - simple reverse echo server built with spring boot
--- src/test/java/com/ccreanga/echoserver/ReverseEchoControllerTests.java
+++ src/test/java/com/ccreanga/echoserver/ReverseEchoControllerTests.java
@@ -1,8 +1,5 @@
 package com.ccreanga.echoserver;
 
-import java.util.List;
-import java.util.Map;
-
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,9 +7,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
 import org.springframework.boot.web.server.LocalServerPort;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.HttpStatus;
-import org.springframework.http.ResponseEntity;
 import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 import static org.assertj.core.api.BDDAssertions.then;
@@ -32,7 +26,7 @@ public class ReverseEchoControllerTests {
     private TestRestTemplate testRestTemplate;
 
     @Test
-    public void reverseEcho() throws Exception {
+    public void reverseEcho(){
 
         String body  = testRestTemplate.getForObject("http://localhost:" + port + "/?text=abc", String.class);
         then(body).isEqualTo("cba");
